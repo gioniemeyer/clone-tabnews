c39b1a3ad34d34f5633d5eb062a655541e83761e
Giovanna Niemeyer <giovanna.niemeyer@gmail.com>
2025-05-03 18:22:50 +0000

    adicionando pasta pages

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <>
+        <h1>O que a vida quer da gente é coragem</h1>
+        <p>João Guimarães Rosa</p>
+        </>
+    )
+}
+
+export default Home;
